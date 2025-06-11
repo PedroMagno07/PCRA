c50ba8ef2109e1aff29aa8c7c6eea22467782b7c
Pedro Magno <134389782+PedroMagno07@users.noreply.github.com>
2025-06-11 00:07:38 -0300

    Add files via upload

--- MB.py
+++ MB.py
@@ -6,6 +6,14 @@ bebidas = [
     {"id": 5,"item": "Redbull", "quantidade": 2, "preço": 13.99}
 ]
 
+moedas = [
+    {"valor": 5, "quantidade": 1},
+    {"valor": 1, "quantidade": 1},
+    {"valor": 0.20, "quantidade": 1},
+    {"valor": 0.10, "quantidade": 1},
+    {"valor": 0.01, "quantidade": 3}
+]
+
 dinheiro = 0
 
 loop = True
@@ -16,45 +24,46 @@ def mostrar_estoque():
         print(f'{produto['id']} | {produto['item'].capitalize()} | {produto['quantidade']} | R${produto['preço']}')
 
 
-print(mostrar_estoque())
+mostrar_estoque()
 
-id = int(input(f'\nSelecione uma das opções disponíveis: ')) #entrada da id do item selecionado pelo user
-preco = 0
-user_bebida = None
+while True:
 
+    id = int(input(f'\nSelecione uma das opções disponíveis: '))  # entrada da id do item selecionado pelo user
+    preco = 0
+    user_bebida = None
+    pago = 0
 
-for item in bebidas:
-    if item["id"] == id: #se produto selecionado por id existe na matriz
-        preco = item["preço"] #definindo a variável preço
-        user_bebida = item["item"]
+    for item in bebidas:
+        if item["id"] == id:  # se produto selecionado por id existe na matriz
+            preco = item["preço"]  # definindo a variável preço
+            user_bebida = item
 
+    if user_bebida is None:
+        print("\nID inválido.")
 
-    #PAGAMENTO
+    elif user_bebida["quantidade"] <= 0:
+        print(f'Item Esgotado.')
 
+    else:
 
+        print(f'\nVocê Selecionou: {user_bebida["item"]} - R${preco:.2f}')
+        pago = float(input(f'R$: '))
+        pago += dinheiro
 
-if preco is None:
-        print("ID inválido. Produto não encontrado.")
+while pago < preco:  # Loop infinito até o pagamento ser válido
+        dinheiro = float(input(f'\n Faltam R${preco - pago:.2f}.\nInsira o valor faltante:\n'))
+        faltam = preco - pago
+        print(f'Valor: R${faltam:.2f}')
+
+if pago > preco:
+
+    troco = pago - preco
+    print(f'\n'
+            f'Seu troco é de: R${troco:.2f}')
+    print('\nPagamento Válido')
+    user_bebida["quantidade"] -= 1
+
 else:
-        dinheiro = float(input(f'\n'
-                               f'Pague o produto selecionado:\n'
-                               f'{user_bebida} R${preco:.2f}: '))  # Mostra o preço correto
-while True:  # Loop infinito até o pagamento ser válido
+    print('\nPagamento Válido')
+    user_bebida["quantidade"] -= 1
 
-            #NÃO ESTÁ ACUMULANDO O VALOR PAGOOOOOOOOOOOOOOOOOOOOOOOOOO
-        if dinheiro < preco:
-                faltam = preco - dinheiro
-                print(f'\n'
-                      f'Faltam: R${faltam}')
-
-        if dinheiro > preco:
-
-                troco = dinheiro - preco
-                print(f'\n'
-                      f'Seu troco é de: R${troco:.2f}')
-                print('Pagamento Válido')
-
-        else:
-
-                print('Pagamento Válido')
-        break
